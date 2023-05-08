6f7b949795428def6c643bcff382bad9c41cea27
Rodrigo Nicola <rodrigo.nicola@softrh.com.br>
2023-05-08 16:41:41 -0300

    Retirado mencoes a produto errado.

--- docker/config/configure.sh
+++ docker/config/configure.sh
@@ -4,9 +4,3 @@
 
 # Gera novo arquivo do zope.conf a partir do .empty
 cp ${INSTANCE_PATH}/etc/zope.conf.empty ${INSTANCE_PATH}/etc/zope.conf
-
-URL_DB=$(cat /run/secrets/zope_secrets | grep -oP '(?<=\bURL_DB=)[^;]+' | sed -e 's/[@]/\\@/g')
-sed -i "s@\${URL_DB}@${URL_DB}@" ${INSTANCE_PATH}/etc/zope.conf
-
-ENCODING_DB=$(cat /run/secrets/zope_secrets | grep -oP '(?<=\bENCODING_DB=)[^;]+' | sed -e 's/[@]/\\@/g')
-sed -i "s@\${ENCODING_DB}@${ENCODING_DB}@" ${INSTANCE_PATH}/etc/zope.conf
